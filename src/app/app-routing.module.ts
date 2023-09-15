fd00d5a02d3e44873f4599eb48eb3693168bfb15
rdlopes17 <rauldlopes@gmail.com>
2023-09-15 17:20:18 -0300

    commit base do projeto-hb-vendas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarCadastroComponent } from './componentes/cadastro/criar-cadastro/criar-cadastro.component';
+import { ListarCadastroComponent } from './componentes/cadastro/listar-cadastro/listar-cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listarCadastro',
+    pathMatch: 'full'
+  },
+  {
+    path: 'criarCadastro',
+    component: CriarCadastroComponent
+  },
+
+  {
+    path: 'listarCadastro',
+    component: ListarCadastroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarCadastroComponent } from './componentes/cadastro/criar-cadastro/criar-cadastro.component';
+import { FormsModule } from '@angular/forms';
+import { ListarCadastroComponent } from './componentes/cadastro/listar-cadastro/listar-cadastro.component';
+import { ItemCadastradoComponent } from './componentes/cadastro/item-cadastrado/item-cadastrado.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarCadastroComponent,
+    ListarCadastroComponent,
+    ItemCadastradoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
